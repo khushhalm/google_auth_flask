5ebd001d7649d85e8ddc26acff23795dd26ed015
Khushhal Maheshwari <khushhal000maheshwari@gmail.com>
2019-05-23 23:14:35 +0530

    flask app having google auth on every route

---
+++ app.py
@@ -0,0 +1,55 @@
+from functools import wraps
+import json
+import os
+
+from flask import Flask, redirect
+
+from authlib.client import OAuth2Session
+import google.oauth2.credentials
+import googleapiclient.discovery
+
+import google_auth
+
+from cred import secret_key
+
+app = Flask(__name__)
+app.secret_key = secret_key
+
+app.register_blueprint(google_auth.app)
+
+
+def login_req(f):
+    @wraps(f)
+    def _secured(*args, **kwargs):
+        if google_auth.is_logged_in():
+            pass
+        else:
+            #return redirect(url_for('/google/login', next=request.url))
+            return redirect("/google/login")
+        return f(*args, **kwargs)
+
+    return _secured
+
+@app.route('/')
+@login_req
+def index():
+    
+    if google_auth.is_logged_in():
+        user_info = google_auth.get_user_info()
+        return '<div>You are currently logged in as ' + user_info['given_name'] + '<div><pre>' + json.dumps(user_info, indent=4) + "</pre>"
+
+    return 'You are not currently logged in.'
+    """
+    user_info = google_auth.get_user_info()
+    return '<div>You are currently logged in as ' + user_info['given_name'] + '<div><pre>' + json.dumps(user_info, indent=4) + "</pre>"
+    """
+
+@app.route('/faltu')
+@login_req
+def faltu():
+
+    return 'You are not currently logged in.'
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=8040, debug=True)
---
+++ cred.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Security Credentials are stored here which are required by app.py
+"""
+
+__author__ = "khushhalm"
+
+GOOGLE_OAUTH_CLIENT_ID = ""
+GOOGLE_OAUTH_CLIENT_SECRET = ""
+secret_key = ''
+
+authorized_emails = [
+    "abc@abc.com",
+    "khushhal000maheshwari@gmail.com"
+    ]
+
+AUTH_REDIRECT_URI = "http://localhost:8040/google/auth"
+BASE_URI = "http://localhost:8040"
